306e623d77a1fe756033051eed0a69b5f1af3793
Andrey Medvedev <andr.medv.spb@gmail.com>
2016-07-05 15:45:11 +0300

    Create two pages app
    Dashboard and home with handlebars templates

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/dashboard', function (req, res) {
+    res.render('dashboard');
+});
+
+app.listen(3000);
