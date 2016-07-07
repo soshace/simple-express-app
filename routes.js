6e688acf0e752ab0d687530a48be70bf387bd0fd
Andrey Medvedev <andr.medv.spb@gmail.com>
2016-07-07 17:44:32 +0300

    Add routes in separate files
    iss: #2114

---
+++ routes.js
@@ -0,0 +1,31 @@
+"use strict"
+
+exports = module.exports = function(app, passport) {
+
+// Generic error handler used by all enpoints
+function handleError(res, reason, message, code) {
+  console.log('Error: ' + reason);
+  res.status(code || 500).json({'error': message});
+}
+
+app.get('/', function (req, res) {
+    // res.render('home', {cat: catMen.name});
+  res.redirect('/dashboard');
+});
+
+app.get('/dashboard', require('./views/dashboard/index').init);
+
+app.get('/dashboard/developers', require('./views/dashboard/developers/index').init);
+app.get('/dashboard/developers/:id', require('./views/dashboard/developers/index').get);
+
+app.post('/dashboard/developers', require('./views/dashboard/developers/index').save);
+  // //front end
+  // app.get('/', require('./views/index').init);
+  // app.get('/about/', require('./views/about/index').init);
+  // app.get('/contact/', require('./views/contact/index').init);
+  // app.post('/contact/', require('./views/contact/index').sendMessage);
+
+  // //sign up
+  // app.get('/signup/', require('./views/signup/index').init);
+  // app.post('/signup/', require('./views/signup/index').signup);
+};
